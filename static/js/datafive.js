7df3aaf2d76b8f9e948884a335d4efe5a19c55b6
Bobby Huffstetler <63428077+bhuffstetler@users.noreply.github.com>
2020-09-25 09:43:25 -0400

    Create datafive.js

---
+++ static/js/datafive.js
@@ -0,0 +1,522 @@
+var data = [
+{
+   "State": "AL",
+   "State_name": "Alabama",
+   "FIPS": 1000,
+   "Breast_incd": 123.3,
+   "colon_rectum_incd": 42.7,
+   "Lung_incd": 63.6,
+   "Melanoma_incd": 22.4,
+   "prostate_incd": 118.7
+ },
+ {
+   "State": "AK",
+   "State_name": "Alaska",
+   "FIPS": 2900,
+   "Breast_incd": 122,
+   "colon_rectum_incd": 42.5,
+   "Lung_incd": 53.5,
+   "Melanoma_incd": 12.1,
+   "prostate_incd": 61
+ },
+ {
+   "State": "AZ",
+   "State_name": "Arizona",
+   "FIPS": 4000,
+   "Breast_incd": 110.9,
+   "colon_rectum_incd": 31.8,
+   "Lung_incd": 46.5,
+   "Melanoma_incd": 23.3,
+   "prostate_incd": 78
+ },
+ {
+   "State": "AR",
+   "State_name": "Arkansas",
+   "FIPS": 5000,
+   "Breast_incd": 117.1,
+   "colon_rectum_incd": 45,
+   "Lung_incd": 77.3,
+   "Melanoma_incd": 22.3,
+   "prostate_incd": 115
+ },
+ {
+   "State": "CA",
+   "State_name": "California",
+   "FIPS": 6000,
+   "Breast_incd": 121.4,
+   "colon_rectum_incd": 35,
+   "Lung_incd": 41,
+   "Melanoma_incd": 22.9,
+   "prostate_incd": 87.1
+ },
+ {
+   "State": "CO",
+   "State_name": "Colorado",
+   "FIPS": 8000,
+   "Breast_incd": 123.4,
+   "colon_rectum_incd": 32.9,
+   "Lung_incd": 40.5,
+   "Melanoma_incd": 21.2,
+   "prostate_incd": 81.6
+ },
+ {
+   "State": "CT",
+   "State_name": "Connecticut",
+   "FIPS": 9000,
+   "Breast_incd": 143.9,
+   "colon_rectum_incd": 35.3,
+   "Lung_incd": 57.2,
+   "Melanoma_incd": 20.6,
+   "prostate_incd": 108.8
+ },
+ {
+   "State": "DE",
+   "State_name": "Delaware",
+   "FIPS": 10000,
+   "Breast_incd": 136.2,
+   "colon_rectum_incd": 39.4,
+   "Lung_incd": 71.8,
+   "Melanoma_incd": 29.5,
+   "prostate_incd": 124.5
+ },
+ {
+   "State": "DC",
+   "State_name": "District of Columbia",
+   "FIPS": 11001,
+   "Breast_incd": 121.1,
+   "colon_rectum_incd": 31,
+   "Lung_incd": 42.5,
+   "Melanoma_incd": 7.8,
+   "prostate_incd": 103.2
+ },
+ {
+   "State": "FL",
+   "State_name": "Florida",
+   "FIPS": 12000,
+   "Breast_incd": 113.8,
+   "colon_rectum_incd": 34.9,
+   "Lung_incd": 54.9,
+   "Melanoma_incd": 23.7,
+   "prostate_incd": 78.2
+ },
+ {
+   "State": "GA",
+   "State_name": "Georgia",
+   "FIPS": 13000,
+   "Breast_incd": 126.4,
+   "colon_rectum_incd": 41.7,
+   "Lung_incd": 60.5,
+   "Melanoma_incd": 27.1,
+   "prostate_incd": 115.2
+ },
+ {
+   "State": "HI",
+   "State_name": "Hawaii",
+   "FIPS": 15000,
+   "Breast_incd": 134.8,
+   "colon_rectum_incd": 41.3,
+   "Lung_incd": 43.8,
+   "Melanoma_incd": 23.1,
+   "prostate_incd": 87.4
+ },
+ {
+   "State": "ID",
+   "State_name": "Idaho",
+   "FIPS": 16000,
+   "Breast_incd": 124.7,
+   "colon_rectum_incd": 35.3,
+   "Lung_incd": 49.7,
+   "Melanoma_incd": 28,
+   "prostate_incd": 98.7
+ },
+ {
+   "State": "IL",
+   "State_name": "Illinois",
+   "FIPS": 17000,
+   "Breast_incd": 134.5,
+   "colon_rectum_incd": 42.8,
+   "Lung_incd": 64,
+   "Melanoma_incd": 20.6,
+   "prostate_incd": 106.8
+ },
+ {
+   "State": "IN",
+   "State_name": "Indiana",
+   "FIPS": 18000,
+   "Breast_incd": 121.3,
+   "colon_rectum_incd": 41.6,
+   "Lung_incd": 72.3,
+   "Melanoma_incd": 20.5,
+   "prostate_incd": 83.3
+ },
+ {
+   "State": "IA",
+   "State_name": "Iowa",
+   "FIPS": 19000,
+   "Breast_incd": 129,
+   "colon_rectum_incd": 43.6,
+   "Lung_incd": 62.7,
+   "Melanoma_incd": 26.5,
+   "prostate_incd": 103.4
+ },
+ {
+   "State": "KS",
+   "State_name": "Kansas",
+   "FIPS": 20000,
+   "Breast_incd": 129.7,
+   "colon_rectum_incd": 38.3,
+   "Lung_incd": 58.8,
+   "Melanoma_incd": 25.3,
+   "prostate_incd": 101.8
+ },
+ {
+   "State": "KY",
+   "State_name": "Kentucky",
+   "FIPS": 21000,
+   "Breast_incd": 126.8,
+   "colon_rectum_incd": 48.4,
+   "Lung_incd": 89.6,
+   "Melanoma_incd": 27.1,
+   "prostate_incd": 99.2
+ },
+ {
+   "State": "LA",
+   "State_name": "Louisiana",
+   "FIPS": 22000,
+   "Breast_incd": 126.6,
+   "colon_rectum_incd": 44.4,
+   "Lung_incd": 65,
+   "Melanoma_incd": 18.8,
+   "prostate_incd": 125.6
+ },
+ {
+   "State": "ME",
+   "State_name": "Maine",
+   "FIPS": 23000,
+   "Breast_incd": 124.5,
+   "colon_rectum_incd": 35.7,
+   "Lung_incd": 69.7,
+   "Melanoma_incd": 28,
+   "prostate_incd": 88.3
+ },
+ {
+   "State": "MD",
+   "State_name": "Maryland",
+   "FIPS": 24000,
+   "Breast_incd": 131.4,
+   "colon_rectum_incd": 36,
+   "Lung_incd": 55.5,
+   "Melanoma_incd": 25.8,
+   "prostate_incd": 118.3
+ },
+ {
+   "State": "MA",
+   "State_name": "Massachusetts",
+   "FIPS": 25000,
+   "Breast_incd": 137.7,
+   "colon_rectum_incd": 35.5,
+   "Lung_incd": 61.8,
+   "Melanoma_incd": 18,
+   "prostate_incd": 98.5
+ },
+ {
+   "State": "MI",
+   "State_name": "Michigan",
+   "FIPS": 26000,
+   "Breast_incd": 119,
+   "colon_rectum_incd": 35.4,
+   "Lung_incd": 62.4,
+   "Melanoma_incd": 20.2,
+   "prostate_incd": 102.8
+ },
+ {
+   "State": "MN",
+   "State_name": "Minnesota",
+   "FIPS": 27000,
+   "Breast_incd": 134.5,
+   "colon_rectum_incd": 38.4,
+   "Lung_incd": 57.1,
+   "Melanoma_incd": 31.6,
+   "prostate_incd": 111.1
+ },
+ {
+   "State": "MS",
+   "State_name": "Mississippi",
+   "FIPS": 28000,
+   "Breast_incd": 117.3,
+   "colon_rectum_incd": 48.1,
+   "Lung_incd": 72.3,
+   "Melanoma_incd": 15.5,
+   "prostate_incd": 126.7
+ },
+ {
+   "State": "MO",
+   "State_name": "Missouri",
+   "FIPS": 29000,
+   "Breast_incd": 129.9,
+   "colon_rectum_incd": 39.8,
+   "Lung_incd": 71,
+   "Melanoma_incd": 19.8,
+   "prostate_incd": 90.6
+ },
+ {
+   "State": "MT",
+   "State_name": "Montana",
+   "FIPS": 30000,
+   "Breast_incd": 130.1,
+   "colon_rectum_incd": 38.9,
+   "Lung_incd": 52.1,
+   "Melanoma_incd": 25.2,
+   "prostate_incd": 107.8
+ },
+ {
+   "State": "NE",
+   "State_name": "Nebraska",
+   "FIPS": 31000,
+   "Breast_incd": 127.7,
+   "colon_rectum_incd": 42.8,
+   "Lung_incd": 57.1,
+   "Melanoma_incd": 25.6,
+   "prostate_incd": 109.3
+ },
+ {
+   "State": "NV",
+   "State_name": "Nevada",
+   "FIPS": 32000,
+   "Breast_incd": 104.7,
+   "colon_rectum_incd": 32.3,
+   "Lung_incd": 46.6,
+   "Melanoma_incd": 11.6,
+   "prostate_incd": 69.5
+ },
+ {
+   "State": "NH",
+   "State_name": "New Hampshire",
+   "FIPS": 33000,
+   "Breast_incd": 145.9,
+   "colon_rectum_incd": 36,
+   "Lung_incd": 62.1,
+   "Melanoma_incd": 31.6,
+   "prostate_incd": 98.7
+ },
+ {
+   "State": "NJ",
+   "State_name": "New Jersey",
+   "FIPS": 34000,
+   "Breast_incd": 134.8,
+   "colon_rectum_incd": 40.5,
+   "Lung_incd": 55.2,
+   "Melanoma_incd": 22.2,
+   "prostate_incd": 127.2
+ },
+ {
+   "State": "NM",
+   "State_name": "New Mexico",
+   "FIPS": 35000,
+   "Breast_incd": 107.8,
+   "colon_rectum_incd": 31.7,
+   "Lung_incd": 38.2,
+   "Melanoma_incd": 16,
+   "prostate_incd": 72.6
+ },
+ {
+   "State": "NY",
+   "State_name": "New York",
+   "FIPS": 36000,
+   "Breast_incd": 134,
+   "colon_rectum_incd": 38.7,
+   "Lung_incd": 59.2,
+   "Melanoma_incd": 17.7,
+   "prostate_incd": 121.9
+ },
+ {
+   "State": "NC",
+   "State_name": "North Carolina",
+   "FIPS": 37000,
+   "Breast_incd": 132.6,
+   "colon_rectum_incd": 37.3,
+   "Lung_incd": 67.2,
+   "Melanoma_incd": 26.8,
+   "prostate_incd": 111.6
+ },
+ {
+   "State": "ND",
+   "State_name": "North Dakota",
+   "FIPS": 38000,
+   "Breast_incd": 133.1,
+   "colon_rectum_incd": 42.4,
+   "Lung_incd": 54.4,
+   "Melanoma_incd": 25.8,
+   "prostate_incd": 125
+ },
+ {
+   "State": "OH",
+   "State_name": "Ohio",
+   "FIPS": 39000,
+   "Breast_incd": 127.9,
+   "colon_rectum_incd": 42.3,
+   "Lung_incd": 67.2,
+   "Melanoma_incd": 25.4,
+   "prostate_incd": 99.2
+ },
+ {
+   "State": "OK",
+   "State_name": "Oklahoma",
+   "FIPS": 40000,
+   "Breast_incd": 121.5,
+   "colon_rectum_incd": 40.8,
+   "Lung_incd": 66.8,
+   "Melanoma_incd": 21.5,
+   "prostate_incd": 93.4
+ },
+ {
+   "State": "OR",
+   "State_name": "Oregon",
+   "FIPS": 41000,
+   "Breast_incd": 115.6,
+   "colon_rectum_incd": 31.9,
+   "Lung_incd": 48.9,
+   "Melanoma_incd": 25.8,
+   "prostate_incd": 86.2
+ },
+ {
+   "State": "PA",
+   "State_name": "Pennsylvania",
+   "FIPS": 42000,
+   "Breast_incd": 131.3,
+   "colon_rectum_incd": 41.9,
+   "Lung_incd": 63.2,
+   "Melanoma_incd": 25.7,
+   "prostate_incd": 104.5
+ },
+ {
+   "State": "RI",
+   "State_name": "Rhode Island",
+   "FIPS": 44000,
+   "Breast_incd": 136.6,
+   "colon_rectum_incd": 31.8,
+   "Lung_incd": 69.2,
+   "Melanoma_incd": 23.5,
+   "prostate_incd": 94.3
+ },
+ {
+   "State": "SC",
+   "State_name": "South Carolina",
+   "FIPS": 45000,
+   "Breast_incd": 129.9,
+   "colon_rectum_incd": 39.7,
+   "Lung_incd": 64.5,
+   "Melanoma_incd": 22.5,
+   "prostate_incd": 116.3
+ },
+ {
+   "State": "SD",
+   "State_name": "South Dakota",
+   "FIPS": 46000,
+   "Breast_incd": 127.4,
+   "colon_rectum_incd": 40,
+   "Lung_incd": 56,
+   "Melanoma_incd": 24.4,
+   "prostate_incd": 113.4
+ },
+ {
+   "State": "TN",
+   "State_name": "Tennessee",
+   "FIPS": 47000,
+   "Breast_incd": 122.2,
+   "colon_rectum_incd": 39.7,
+   "Lung_incd": 76.6,
+   "Melanoma_incd": 21.9,
+   "prostate_incd": 107.9
+ },
+ {
+   "State": "TX",
+   "State_name": "Texas",
+   "FIPS": 48000,
+   "Breast_incd": 112.2,
+   "colon_rectum_incd": 37.5,
+   "Lung_incd": 49.3,
+   "Melanoma_incd": 13,
+   "prostate_incd": 86.5
+ },
+ {
+   "State": "UT",
+   "State_name": "Utah",
+   "FIPS": 49000,
+   "Breast_incd": 113.4,
+   "colon_rectum_incd": 27.9,
+   "Lung_incd": 25.5,
+   "Melanoma_incd": 40.5,
+   "prostate_incd": 113.2
+ },
+ {
+   "State": "VT",
+   "State_name": "Vermont",
+   "FIPS": 50000,
+   "Breast_incd": 130.5,
+   "colon_rectum_incd": 33.8,
+   "Lung_incd": 60.4,
+   "Melanoma_incd": 35.8,
+   "prostate_incd": 82.5
+ },
+ {
+   "State": "VA",
+   "State_name": "Virginia",
+   "FIPS": 51000,
+   "Breast_incd": 124.5,
+   "colon_rectum_incd": 33.7,
+   "Lung_incd": 55,
+   "Melanoma_incd": 18.6,
+   "prostate_incd": 94.8
+ },
+ {
+   "State": "WA",
+   "State_name": "Washington",
+   "FIPS": 53000,
+   "Breast_incd": 132,
+   "colon_rectum_incd": 33.5,
+   "Lung_incd": 52.8,
+   "Melanoma_incd": 26.1,
+   "prostate_incd": 91.7
+ },
+ {
+   "State": "WV",
+   "State_name": "West Virginia",
+   "FIPS": 54000,
+   "Breast_incd": 119.6,
+   "colon_rectum_incd": 47.1,
+   "Lung_incd": 77.4,
+   "Melanoma_incd": 20.8,
+   "prostate_incd": 88.1
+ },
+ {
+   "State": "WI",
+   "State_name": "Wisconsin",
+   "FIPS": 55000,
+   "Breast_incd": 129,
+   "colon_rectum_incd": 35.8,
+   "Lung_incd": 57.4,
+   "Melanoma_incd": 23.6,
+   "prostate_incd": 107.1
+ },
+ {
+   "State": "WY",
+   "State_name": "Wyoming",
+   "FIPS": 56000,
+   "Breast_incd": 104,
+   "colon_rectum_incd": 30.3,
+   "Lung_incd": 42.4,
+   "Melanoma_incd": 23,
+   "prostate_incd": 96.7
+ },
+ {
+   "State": "PR",
+   "State_name": "Puerto Rico",
+   "FIPS": 72001,
+   "Breast_incd": 93.1,
+   "colon_rectum_incd": 41.8,
+   "Lung_incd": 16.6,
+   "Melanoma_incd": 3.1,
+   "prostate_incd": 144.7
+ }
+]
