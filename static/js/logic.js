c911db20d2bc673f520dc9417f72f32257b21f44
James Auman <jtauman@nc.rr.com>
2020-09-29 19:24:12 -0400

    updated json files, index_map.html, logic.js, config.js, style.css

---
+++ static/js/logic.js
@@ -0,0 +1,48 @@
+// Create a map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Use this link to get the geojson data.
+var link = "data/gz_2010_us_040_00_20m.json";
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Called on each feature
+    onEachFeature: function (feature, layer) {
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 20%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.2
+          });
+        },
+        // When a feature (state) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        }
+      });
+      layer.bindPopup("<h1>" + feature.properties.NAME + "</h1>")
+    }
+  }).addTo(myMap);
+});
