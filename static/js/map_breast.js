15bc2618e4eb6108db7de001115580fbb38444a1
Bobby Huffstetler <63428077+bhuffstetler@users.noreply.github.com>
2020-09-29 20:42:34 -0400

    Add files via upload

---
+++ static/js/map_breast.js
@@ -0,0 +1,211 @@
+// code at the bottom
+riskData = [{"Fruit %":59.8,"Healthy BMI age 20 plus %":32.2,"No physical activity %":24.2,"Obese BMI age 20 plus %":30.2,"Obese BMI highschool %":14.8,"Overweight BMI %":15.6,"Smoking %":16.4,"State":"United States","Veggies %":77.9},{"Fruit %":51.7,"Healthy BMI age 20 plus %":27.6,"No physical activity %":29.4,"Obese BMI age 20 plus %":36.4,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":21.5,"State":"Alabama","Veggies %":72.2},{"Fruit %":59.0,"Healthy BMI age 20 plus %":30.2,"No physical activity %":19.1,"Obese BMI age 20 plus %":32.5,"Obese BMI highschool %":13.7,"Overweight BMI %":17.5,"Smoking %":19.0,"State":"Alaska","Veggies %":81.1},{"Fruit %":60.3,"Healthy BMI age 20 plus %":32.9,"No physical activity %":23.1,"Obese BMI age 20 plus %":29.9,"Obese BMI highschool %":12.3,"Overweight BMI %":15.9,"Smoking %":14.7,"State":"Arizona","Veggies %":79.5},{"Fruit %":50.4,"Healthy BMI age 20 plus %":28.1,"No physical activity %":32.5,"Obese BMI age 20 plus %":36.5,"Obese BMI highschool %":21.7,"Overweight BMI %":18.1,"Smoking %":23.6,"State":"Arkansas","Veggies %":71.9},{"Fruit %":64.2,"Healthy BMI age 20 plus %":35.5,"No physical activity %":20.5,"Obese BMI age 20 plus %":25.5,"Obese BMI highschool %":13.9,"Overweight BMI %":15.0,"Smoking %":11.0,"State":"California","Veggies %":81.4},{"Fruit %":64.2,"Healthy BMI age 20 plus %":38.9,"No physical activity %":15.8,"Obese BMI age 20 plus %":22.8,"Obese BMI highschool %":9.5,"Overweight BMI %":12.3,"Smoking %":15.6,"State":"Colorado","Veggies %":82.2},{"Fruit %":64.5,"Healthy BMI age 20 plus %":36.0,"No physical activity %":21.3,"Obese BMI age 20 plus %":26.6,"Obese BMI highschool %":12.7,"Overweight BMI %":16.0,"Smoking %":13.3,"State":"Connecticut","Veggies %":80.5},{"Fruit %":61.8,"Healthy BMI age 20 plus %":29.0,"No physical activity %":26.6,"Obese BMI age 20 plus %":31.5,"Obese BMI highschool %":15.1,"Overweight BMI %":16.6,"Smoking %":17.7,"State":"Delaware","Veggies %":79.3},{"Fruit %":63.0,"Healthy BMI age 20 plus %":43.3,"No physical activity %":16.2,"Obese BMI age 20 plus %":23.2,"Obese BMI highschool %":16.8,"Overweight BMI %":18.0,"Smoking %":14.7,"State":"District of Columbia","Veggies %":81.0},{"Fruit %":61.0,"Healthy BMI age 20 plus %":33.5,"No physical activity %":29.8,"Obese BMI age 20 plus %":28.0,"Obese BMI highschool %":10.9,"Overweight BMI %":14.2,"Smoking %":15.5,"State":"Florida","Veggies %":78.7},{"Fruit %":55.2,"Healthy BMI age 20 plus %":31.3,"No physical activity %":29.4,"Obese BMI age 20 plus %":32.1,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":17.9,"State":"Georgia","Veggies %":75.3},{"Fruit %":58.7,"Healthy BMI age 20 plus %":38.8,"No physical activity %":20.8,"Obese BMI age 20 plus %":24.4,"Obese BMI highschool %":14.2,"Overweight BMI %":14.2,"Smoking %":13.1,"State":"Hawaii","Veggies %":78.6},{"Fruit %":60.6,"Healthy BMI age 20 plus %":33.0,"No physical activity %":20.2,"Obese BMI age 20 plus %":27.9,"Obese BMI highschool %":11.4,"Overweight BMI %":14.7,"Smoking %":14.5,"State":"Idaho","Veggies %":81.4},{"Fruit %":61.5,"Healthy BMI age 20 plus %":31.6,"No physical activity %":23.9,"Obese BMI age 20 plus %":32.6,"Obese BMI highschool %":14.8,"Overweight BMI %":16.1,"Smoking %":15.8,"State":"Illinois","Veggies %":75.7},{"Fruit %":57.2,"Healthy BMI age 20 plus %":29.9,"No physical activity %":26.8,"Obese BMI age 20 plus %":33.2,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":21.1,"State":"Indiana","Veggies %":73.3},{"Fruit %":58.3,"Healthy BMI age 20 plus %":28.6,"No physical activity %":22.7,"Obese BMI age 20 plus %":33.2,"Obese BMI highschool %":15.3,"Overweight BMI %":16.0,"Smoking %":16.7,"State":"Iowa","Veggies %":73.1},{"Fruit %":56.3,"Healthy BMI age 20 plus %":30.5,"No physical activity %":23.5,"Obese BMI age 20 plus %":32.0,"Obese BMI highschool %":13.1,"Overweight BMI %":15.3,"Smoking %":17.2,"State":"Kansas","Veggies %":77.7},{"Fruit %":53.1,"Healthy BMI age 20 plus %":28.2,"No physical activity %":29.8,"Obese BMI age 20 plus %":34.9,"Obese BMI highschool %":20.2,"Overweight BMI %":16.1,"Smoking %":24.5,"State":"Kentucky","Veggies %":75.4},{"Fruit %":49.7,"Healthy BMI age 20 plus %":28.1,"No physical activity %":29.1,"Obese BMI age 20 plus %":36.0,"Obese BMI highschool %":17.0,"Overweight BMI %":18.3,"Smoking %":22.8,"State":"Louisiana","Veggies %":69.0},{"Fruit %":64.8,"Healthy BMI age 20 plus %":32.6,"No physical activity %":20.6,"Obese BMI age 20 plus %":30.4,"Obese BMI highschool %":14.3,"Overweight BMI %":16.0,"Smoking %":19.8,"State":"Maine","Veggies %":81.7},{"Fruit %":64.0,"Healthy BMI age 20 plus %":32.8,"No physical activity %":23.1,"Obese BMI age 20 plus %":30.6,"Obese BMI highschool %":12.6,"Overweight BMI %":15.2,"Smoking %":13.7,"State":"Maryland","Veggies %":78.7},{"Fruit %":66.0,"Healthy BMI age 20 plus %":36.6,"No physical activity %":20.0,"Obese BMI age 20 plus %":24.1,"Obese BMI highschool %":11.7,"Overweight BMI %":14.0,"Smoking %":13.6,"State":"Massachusetts","Veggies %":81.7},{"Fruit %":60.3,"Healthy BMI age 20 plus %":30.0,"No physical activity %":23.9,"Obese BMI age 20 plus %":33.2,"Obese BMI highschool %":16.7,"Overweight BMI %":16.3,"Smoking %":20.4,"State":"Michigan","Veggies %":75.3},{"Fruit %":62.9,"Healthy BMI age 20 plus %":32.3,"No physical activity %":18.0,"Obese BMI age 20 plus %":28.5,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":15.2,"State":"Minnesota","Veggies %":77.6},{"Fruit %":48.8,"Healthy BMI age 20 plus %":25.8,"No physical activity %":30.3,"Obese BMI age 20 plus %":38.0,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":22.7,"State":"Mississippi","Veggies %":68.8},{"Fruit %":55.6,"Healthy BMI age 20 plus %":30.2,"No physical activity %":24.9,"Obese BMI age 20 plus %":32.3,"Obese BMI highschool %":16.6,"Overweight BMI %":15.7,"Smoking %":22.1,"State":"Missouri","Veggies %":76.7},{"Fruit %":60.7,"Healthy BMI age 20 plus %":34.7,"No physical activity %":19.9,"Obese BMI age 20 plus %":26.1,"Obese BMI highschool %":11.7,"Overweight BMI %":14.6,"Smoking %":18.5,"State":"Montana","Veggies %":80.7},{"Fruit %":58.9,"Healthy BMI age 20 plus %":28.7,"No physical activity %":22.4,"Obese BMI age 20 plus %":32.8,"Obese BMI highschool %":14.6,"Overweight BMI %":16.6,"Smoking %":17.0,"State":"Nebraska","Veggies %":75.3},{"Fruit %":63.1,"Healthy BMI age 20 plus %":35.4,"No physical activity %":24.7,"Obese BMI age 20 plus %":26.2,"Obese BMI highschool %":14.0,"Overweight BMI %":14.3,"Smoking %":16.5,"State":"Nevada","Veggies %":80.8},{"Fruit %":66.7,"Healthy BMI age 20 plus %":33.6,"No physical activity %":19.3,"Obese BMI age 20 plus %":27.1,"Obese BMI highschool %":12.8,"Overweight BMI %":14.1,"Smoking %":18.0,"State":"New Hampshire","Veggies %":82.7},{"Fruit %":64.1,"Healthy BMI age 20 plus %":33.1,"No physical activity %":29.8,"Obese BMI age 20 plus %":27.8,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":14.0,"State":"New Jersey","Veggies %":77.9},{"Fruit %":56.9,"Healthy BMI age 20 plus %":31.9,"No physical activity %":20.3,"Obese BMI age 20 plus %":28.9,"Obese BMI highschool %":15.3,"Overweight BMI %":16.4,"Smoking %":16.6,"State":"New Mexico","Veggies %":78.5},{"Fruit %":62.6,"Healthy BMI age 20 plus %":35.9,"No physical activity %":26.3,"Obese BMI age 20 plus %":26.1,"Obese BMI highschool %":12.4,"Overweight BMI %":16.2,"Smoking %":14.2,"State":"New York","Veggies %":77.6},{"Fruit %":56.7,"Healthy BMI age 20 plus %":30.7,"No physical activity %":23.3,"Obese BMI age 20 plus %":32.2,"Obese BMI highschool %":15.4,"Overweight BMI %":15.5,"Smoking %":17.9,"State":"North Carolina","Veggies %":78.4},{"Fruit %":59.6,"Healthy BMI age 20 plus %":29.3,"No physical activity %":22.2,"Obese BMI age 20 plus %":32.5,"Obese BMI highschool %":14.9,"Overweight BMI %":16.2,"Smoking %":19.8,"State":"North Dakota","Veggies %":72.5},{"Fruit %":57.1,"Healthy BMI age 20 plus %":30.9,"No physical activity %":25.9,"Obese BMI age 20 plus %":32.3,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":22.5,"State":"Ohio","Veggies %":75.3},{"Fruit %":48.9,"Healthy BMI age 20 plus %":28.9,"No physical activity %":28.5,"Obese BMI age 20 plus %":33.0,"Obese BMI highschool %":17.1,"Overweight BMI %":16.5,"Smoking %":19.6,"State":"Oklahoma","Veggies %":75.5},{"Fruit %":63.5,"Healthy BMI age 20 plus %":34.4,"No physical activity %":17.2,"Obese BMI age 20 plus %":29.7,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":16.2,"State":"Oregon","Veggies %":83.5},{"Fruit %":60.7,"Healthy BMI age 20 plus %":32.0,"No physical activity %":22.9,"Obese BMI age 20 plus %":31.2,"Obese BMI highschool %":13.7,"Overweight BMI %":15.7,"Smoking %":18.0,"State":"Pennsylvania","Veggies %":75.8},{"Fruit %":44.4,"Healthy BMI age 20 plus %":30.1,"No physical activity %":41.7,"Obese BMI age 20 plus %":31.4,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":10.6,"State":"Puerto Rico","Veggies %":75.5},{"Fruit %":62.4,"Healthy BMI age 20 plus %":33.1,"No physical activity %":24.4,"Obese BMI age 20 plus %":27.5,"Obese BMI highschool %":15.2,"Overweight BMI %":15.9,"Smoking %":14.4,"State":"Rhode Island","Veggies %":76.5},{"Fruit %":52.9,"Healthy BMI age 20 plus %":29.9,"No physical activity %":26.9,"Obese BMI age 20 plus %":32.8,"Obese BMI highschool %":17.2,"Overweight BMI %":16.5,"Smoking %":20.0,"State":"South Carolina","Veggies %":74.6},{"Fruit %":57.5,"Healthy BMI age 20 plus %":30.3,"No physical activity %":18.9,"Obese BMI age 20 plus %":30.5,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":18.1,"State":"South Dakota","Veggies %":73.9},{"Fruit %":54.9,"Healthy BMI age 20 plus %":28.7,"No physical activity %":28.4,"Obese BMI age 20 plus %":35.3,"Obese BMI highschool %":20.5,"Overweight BMI %":17.5,"Smoking %":22.1,"State":"Tennessee","Veggies %":77.4},{"Fruit %":57.5,"Healthy BMI age 20 plus %":28.8,"No physical activity %":25.2,"Obese BMI age 20 plus %":34.1,"Obese BMI highschool %":18.6,"Overweight BMI %":18.0,"Smoking %":14.3,"State":"Texas","Veggies %":80.5},{"Fruit %":62.4,"Healthy BMI age 20 plus %":36.3,"No physical activity %":15.7,"Obese BMI age 20 plus %":26.4,"Obese BMI highschool %":9.6,"Overweight BMI %":13.2,"Smoking %":8.8,"State":"Utah","Veggies %":80.0},{"Fruit %":66.2,"Healthy BMI age 20 plus %":36.1,"No physical activity %":19.5,"Obese BMI age 20 plus %":27.6,"Obese BMI highschool %":12.6,"Overweight BMI %":14.1,"Smoking %":17.0,"State":"Vermont","Veggies %":82.4},{"Fruit %":59.9,"Healthy BMI age 20 plus %":32.2,"No physical activity %":23.3,"Obese BMI age 20 plus %":29.3,"Obese BMI highschool %":12.7,"Overweight BMI %":15.5,"Smoking %":15.3,"State":"Virginia","Veggies %":78.3},{"Fruit %":63.5,"Healthy BMI age 20 plus %":33.4,"No physical activity %":17.6,"Obese BMI age 20 plus %":29.3,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":13.9,"State":"Washington","Veggies %":83.2},{"Fruit %":50.1,"Healthy BMI age 20 plus %":26.6,"No physical activity %":28.5,"Obese BMI age 20 plus %":38.2,"Obese BMI highschool %":19.5,"Overweight BMI %":16.0,"Smoking %":24.8,"State":"West Virginia","Veggies %":73.5},{"Fruit %":62.1,"Healthy BMI age 20 plus %":30.6,"No physical activity %":20.0,"Obese BMI age 20 plus %":31.7,"Obese BMI highschool %":13.7,"Overweight BMI %":15.0,"Smoking %":17.1,"State":"Wisconsin","Veggies %":76.0},{"Fruit %":58.2,"Healthy BMI age 20 plus %":32.5,"No physical activity %":23.1,"Obese BMI age 20 plus %":28.4,"Obese BMI highschool %":NaN,"Overweight BMI %":NaN,"Smoking %":18.9,"State":"Wyoming","Veggies %":78.5}]
+
+console.log(riskData);
+
+function buildMap() {
+    // Creating map object
+    var map = L.map("map", {
+          center: [38.0748599,-94.5554788],
+          zoom: 4.48,
+    //      layers: [cancers]
+    });
+
+    // Adding tile layer
+    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+          attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+          maxZoom: 18,
+          id: "mapbox.streets",
+          accessToken: API_KEY
+    }).addTo(map);
+
+    var link = "/gz_2010_us_040_00_20m";
+
+    // Grabbing our GeoJSON data..
+    d3.json(link, function(data) {
+          // Creating a GeoJSON layer with the retrieved datas
+          L.geoJson(data).addTo(map);
+    });
+
+    
+    var cancerData = "/api/incidence";
+    var risk_information = "/api/risks";
+    
+    
+    
+    
+    // Color shading based on incidence
+    function chooseColor(num) {
+        num = parseInt(num);
+        if (num<2000) {
+            return "#FFE400";
+        } else if (num<4000) {
+            return "#FFB200"
+        } else if (num<6000) {
+            return "#FF8000"
+        } else if (num<8000) {
+            return "#FF6100"
+        } else if (num<10000) {
+            return "#FF2A00"
+        } else {
+            return "red";
+        }
+    }
+         
+
+    
+    d3.json(cancerData, function(data){
+        var state_incident = {};
+        console.log(data);
+        data.forEach(row => state_incident[row.State] = row["Breast cancer incidence"])
+        console.log(state_incident); 
+    
+    var fruit = {}
+    riskData.forEach(row => fruit[row.State] = row["Fruit %"])
+    
+    var veggie = {}
+    riskData.forEach(row => veggie[row.State] = row["Veggies %"])
+
+        
+    var healthyBMI = {}
+    riskData.forEach(row => healthyBMI[row.State] = row["Healthy BMI age 20 plus %"])
+
+    
+    var obeseBMI20 = {}
+    riskData.forEach(row => obeseBMI20[row.State] = row["Obese BMI age 20 plus %"])
+
+    
+    var obeseBMIhs = {}
+    riskData.forEach(row => obeseBMIhs[row.State] = row["Obese BMI highschool %"])
+
+        
+    var overweight = {}
+    riskData.forEach(row => overweight[row.State] = row["Overweight BMI %"])
+
+
+    var smoking = {}
+    riskData.forEach(row => smoking[row.State] = row["Smoking %"])
+
+    
+    var noPhysical = {}
+    riskData.forEach(row => noPhysical[row.State] = row["No physical activity %"])
+        
+
+        
+    // Grabbing our GeoJSON data..
+    d3.json(link, function(data){
+    // Creating a geoJSON layer with the retrieved data
+      L.geoJson(data, {
+        // Style each feature (in this case a neighborhood)
+        style: function(feature) {
+          return {
+            color: "white",
+            // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+            fillColor: chooseColor(state_incident[feature.properties.NAME]),
+            fillOpacity: 0.5,
+            weight: 1.5
+          };
+        console.log(feature.properties.NAME);
+        },
+        // Called on each feature
+        onEachFeature: function(feature, layer) {
+          // Set mouse events to change map styling
+          layer.on({
+            // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+            mouseover: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.9
+              });
+            },
+            // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+            mouseout: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.5
+              });
+            },
+            // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+            click: function(event) {
+              map.fitBounds(event.target.getBounds());
+            }
+          });
+          // Giving each feature a pop-up with information pertinent to it
+          layer.bindPopup("<h2>" + feature.properties.NAME + "</h2> <hr> <p>" + state_incident[feature.properties.NAME] + ' Incidences' + '<br>' + 'Fruit %: ' + fruit[feature.properties.NAME] + 
+                          '<br>' + 'Veggie %: ' + veggie[feature.properties.NAME] +
+                          '<br>' + 'Healthy BMI age 20 plus %: ' + healthyBMI[feature.properties.NAME] +
+                          '<br>' + 'Obese BMI age 20 plus %: ' + obeseBMI20[feature.properties.NAME] +
+                          '<br>' + 'Obese BMI highschool %: ' + obeseBMIhs[feature.properties.NAME] +
+                          '<br>' + 'Overweight BMI %: ' + overweight[feature.properties.NAME] +
+                          '<br>' + 'No physical activity %: ' + noPhysical[feature.properties.NAME] +
+                          '<br>' + 'Smoking %: ' + smoking[feature.properties.NAME] +
+                          "</p>");
+
+        }
+      }).addTo(map);
+
+    
+    });
+    });
+    
+    
+    // add legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            limits = [0, 2000, 4000, 6000, 8000, 10000],
+            colors = ["#FFE400", "#FFB200", "#FF8000", "#FF6100", "#FF2A00", "red"],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < limits.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colors[i] + '"></i> ' +
+                limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+
+    // add info
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+
+        info.update = function () {
+        this._div.innerHTML = '<h4>US Breast Cancer Incidences, 2015</h4>'
+    };
+
+    info.addTo(map);
+
+
+}
+
+
+function init() {
+    buildMap();
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
