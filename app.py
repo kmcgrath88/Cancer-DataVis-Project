a57ada1d9f9c6780bc410d698b1987f437ec83ea
Kristi McGrath <63806822+kmcgrath88@users.noreply.github.com>
2020-10-03 15:24:49 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,48 +1,133 @@
 #Import dependencies and setup
+import os
 import json
 import requests
-from flask import Flask, render_template, redirect
+from bson import json_util
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import cancer_scrape #Bringing in cancer_scrape.py file
-import geopandas
+import pandas as pd
+import geojson
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": "*"
+    }
+})
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_ORIGINS'] = '*'
+
 #Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/cancer_app" 
 mongo = PyMongo(app)
 
 #Creating app route "/" and defining function ---- this works
-# @app.route("/")
-# def index():
-#     cancer_data = mongo.db.cancer_data.find_one()
-#     return render_template("index.html", cancer_data = cancer_data)
-
-#Route to get geojson data -- testing
-@app.route("/", methods = ["GET"])
-#@cross_origin()
+@app.route("/cancer_scrape/")
+@cross_origin()
 def index():
-    # mars_data = scrape_mars.scrape_all()
-    # mars.update({}, mars_data, upsert=True)
-#     alldogs = dogdb.find({})
-#     dogsjson = json.loads(json_util.dumps(alldogs))
+    cancer_data = mongo.db.cancer_data.find_one()
+    cancerjson = json.loads(json_util.dumps(cancer_data))
+    # print(cancerjson)
+    return jsonify(cancerjson)
+    #return render_template("index.html", cancer_data = cancer_data)
 
-   
-    all_cancers = mongo.db.all_cancers
-    response = geopandas.read_file("Data/US_cancer_state.geojson")
+# Route to get geojson data -- testing
+@app.route("/cancer_dash/", methods = ["GET"])
+@cross_origin()
+def index2():
     
-    all_cancers.insert(response)
-    return render_template("index.html", all_cancers = all_cancers)
+    all_cancers = mongo.db.all_cancers
+    # Drops collection if available to remove duplicates
+    all_cancers.drop()
+    # Loading json file
+    with open("Data/US_cancer_state.geojson") as file:
+        file_data = geojson.load(file)
+    #insert data into collection
+    if isinstance(file_data, list):
+        all_cancers.insert_many(file_data)
+    else:
+        all_cancers.insert_one(file_data)
 
-#Creating app route "/scrape" and defining function
+    all_cancers2 = all_cancers.find_one()
+    cancerjson = json.loads(json_util.dumps(all_cancers2))
+    #print(cancerjson)
+    return jsonify(cancerjson)
+    # return render_template("index.html", all_cancers = all_cancers2)
+
+
+#Creating app route "/scrape" and defining function  -- updating mongodb with scraped data; route for loading data in
 @app.route("/scrape")
+@cross_origin()
 def scraper():
+    all_cancers = mongo.db.all_cancers
+    # Drops collection if available to remove duplicates
+    all_cancers.geojson.drop()
+    # Loading json file
+    with open("Data/US_cancer_state.geojson") as file:
+        file_data = geojson.load(file)
+    #insert data into collection
+    if isinstance(file_data, list):
+        all_cancers.insert_many(file_data)
+    else:
+        all_cancers.insert_one(file_data)
     cancer_data  = mongo.db.cancer_data 
+
     cancer_data2 = cancer_scrape.scrape()
     cancer_data.update({}, cancer_data2, upsert=True)
-    return redirect("/")
+    
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+## Nick's example code:
+# from flask import Flask, render_template, redirect, url_for, Response, jsonify
+# from bson import json_util
+# from flask_pymongo import PyMongo
+# from flask_cors import CORS, cross_origin
+# import requests
+# import json
+# app = Flask(__name__)
+# CORS(app, resources={
+#     r"/*": {
+#         "origins": "*"
+#     }
+# })
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# app.config['CORS_ORIGINS'] = '*'
+# # Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/dogs"
+# mongo = PyMongo(app)
+# # Or set inline
+# # mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# #This is not recommended in production
+# #What would happen is every time you visit the root route it would load the DB again with all the data
+# #
+# @app.route("/", methods=["GET"])
+# def index():
+#     dogcollection = mongo.db.alldogs 
+#     response = requests.get("https://dog.ceo/api/breeds/list/all")
+#     # print(response.json())
+#     responseJson = response.json()
+#     dogcollection.insert(responseJson)
+#     # return render_template("index.html", mars=mars)
+# @app.route("/allbreeds/", methods=['GET'])
+# @cross_origin()
+# def allbreeds():
+#     dogdb = mongo.db.alldogs
+#     # mars_data = scrape_mars.scrape_all()
+#     # mars.update({}, mars_data, upsert=True)
+#     alldogs = dogdb.find({})
+#     dogsjson = json.loads(json_util.dumps(alldogs))
+#     return jsonify(dogsjson)
+# if __name__ == "__main__":
+#     app.run()
