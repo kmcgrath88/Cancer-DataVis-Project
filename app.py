97e68fa970e2d73f0f430066cca72fe2d18a7615
Kristi McGrath <mcgrathkristi@gmail.com>
2020-10-07 16:46:34 -0400

    Cleaning up comments

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-#Import dependencies and setup
+# Import dependencies and setup
 import os
 import json
 import requests
 from bson import json_util
 from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
-import cancer_scrape #Bringing in cancer_scrape.py file
+import cancer_scrape # Bringing in cancer_scrape.py file
 import pandas as pd
 import geojson
 from flask_cors import CORS, cross_origin
-
 
 app = Flask(__name__)
 CORS(app, resources={
@@ -20,67 +19,70 @@ CORS(app, resources={
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['CORS_ORIGINS'] = '*'
 
-#Use flask_pymongo to set up mongo connection
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/cancer_app" 
 mongo = PyMongo(app)
 
-#Creating app route "/" and defining function ---- this works
-# @app.route("/cancer_scrape/")
+# Creating app route "/" and defining function
 @app.route("/")
 @cross_origin()
 def index():
     cancer_data = mongo.db.cancer_data.find_one()
-    # cancerjson = json.loads(json_util.dumps(cancer_data))
-    # print(cancerjson)
-    # return jsonify(cancerjson)
     return render_template("index.html", cancer_data = cancer_data)
 
-# Route to get geojson data -- testing
+# Route to get cancer incidence geojson data
 @app.route("/cancer_dash/", methods = ["GET"])
 @cross_origin()
 def index2():
-    
     all_cancers = mongo.db.all_cancers
-    # Drops collection if available to remove duplicates
+
+    # Dropping collection if exists to remove duplicates
     all_cancers.drop()
-    # Loading json file
+
+    # Loading geojson file
     with open("Data/US_cancer_state.geojson") as file:
         file_data = geojson.load(file)
-    #insert data into collection
+    # Inserting data into collection
     if isinstance(file_data, list):
         all_cancers.insert_many(file_data)
     else:
         all_cancers.insert_one(file_data)
 
+    # Finding cancer incidence data
     all_cancers2 = all_cancers.find_one()
+
+    # Parsing json string into python dictionary, returning string
     cancerjson = json.loads(json_util.dumps(all_cancers2))
-    # print(jsonify(cancerjson))
+    
     return jsonify(cancerjson)
-    # return render_template("index.html", all_cancers = all_cancers2)
 
 # Route to get mortality data
 @app.route("/cancer_mortality/", methods = ["GET"])
 @cross_origin()
 def index3():
-    
     mortality = mongo.db.mortality
-    # Drops collection if available to remove duplicates
+
+    # Dropping collection if exists to remove duplicates
     mortality.drop()
+
     # Loading json file
     with open("Data/cancer_mortality_final.json") as file:
         file_data_mort = json.load(file)
-    #insert data into collection
+    # Inserting data into collection
     if isinstance(file_data_mort, list):
         mortality.insert_many(file_data_mort)
     else:
         mortality.insert_one(file_data_mort)
 
+    # Finding cancer mortality data
     mortality2 = mortality.find()
+
+    # Parsing json string into python dictionary, returning string
     mortalityjson = json.loads(json_util.dumps(mortality2))
-    # print(jsonify(cancerjson))
     return jsonify(mortalityjson)
 
-#Creating app route "/scrape" and defining function  -- updating mongodb with scraped data; route for loading data in
+# Creating app route "/scrape" and defining function 
+# Updating mongodb with scraped data - route for loading data in
 @app.route("/scrape")
 @cross_origin()
 def scraper():
@@ -88,10 +90,6 @@ def scraper():
     cancer_data2 = cancer_scrape.scrape()
     cancer_data.update({}, cancer_data2, upsert=True)
     return redirect("/")
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- cancer_scrape.py
+++ cancer_scrape.py
@@ -1,15 +1,15 @@
-#Import dependencies and setup
+# Import dependencies and setup
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import time
 
-#Function to intialize brower
+# Function to intialize brower
 def init_browser():
     executable_path = {"executable_path": "chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
-#Function to scrape information 
+# Function to scrape information - steps from jupyter notebook
 def scrape():
     browser = init_browser()
 
@@ -17,23 +17,25 @@ def scrape():
     url = "https://www.cancer.gov/news-events"
     browser.visit(url)
 
-    time.sleep(1) #Allow page to load
+    # Allow page to load
+    time.sleep(1) 
 
-    #Scrape page into Soup
+    # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
 
+    # Create empty dictionary
     news_data = {}
-    #Get the latest news title and text
+    # Retrieving the latest news title and text, and adding to dictionary
     article = soup.find('div', class_= 'feature-card')
     news_data['article_title'] = article.find('h3').text
-    
     news_data['article_text'] = article.find('p').text
-
+    # Retrieving the news url and adding to dictionary
     news_data['news_url'] = "https://www.cancer.gov" + article.find('a')['href']
 
+    # Click first news article
     browser.find_by_css('div.image-hover').click()
-        
+    # Retrieving date and adding to dictionary
     news_data['date'] = browser.find_by_css('time').text
 
     #Quit the browser after scraping
